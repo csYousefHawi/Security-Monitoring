b6bd65292b152bd06ef520ad59565bde2b75293e
YousefAlhawi <cscyber17@gmail.com>
2026-03-23 16:40:52 +0300

    Update suspicious_process_detector.py

--- Security-Monitoring-Toolkit/suspicious_process_detector.py
+++ Security-Monitoring-Toolkit/suspicious_process_detector.py
@@ -1,4 +1,5 @@
-
+## This tool lists all currently running processes and identifies if any are running as 'ADMIN' (root/administrator).
+## It uses the `psutil` library to list processes and check their user labels.
 def suspicious_process_detector():
     import psutil
 
