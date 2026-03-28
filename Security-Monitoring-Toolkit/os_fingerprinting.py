68b4817732dc6403c47664bdb9b62f6c869ff98c
YousefAlhawi <cscyber17@gmail.com>
2026-03-28 19:58:29 +0300

    Update os_fingerprinting.py

--- Security-Monitoring-Toolkit/os_fingerprinting.py
+++ Security-Monitoring-Toolkit/os_fingerprinting.py
@@ -1,12 +1,25 @@
-## This tool identifies the system's operating system name and version. 
-## It uses the `platform` library to retrieve this information and displays it on the screen.
+## This tool identifies the system's operating system name and version and some information about processor and machine and other. 
+## It uses the platform library to retrieve this information and displays it on the screen.
+
 def os_fingerprinting():
     import platform
+    
 
     os_name = platform.system()
     os_version = platform.version()
-    print(f"OS Name: {os_name}")
-    print(f"OS Version: {os_version}")
-    
-if __name__ == "__main__":
+    machine = platform.machine()
+    processor = platform.processor()
+    release = platform.release()
+    node = platform.node()
+
+
+    print("OS Name: " + os_name)
+    print("OS Version: " + os_version)
+    print("Machine: " + machine)
+    print("Processor: " + processor)
+    print("Release: " + release)
+    print("Node: " + node)
+
+
+if _name_ == "_main_":
     os_fingerprinting()
